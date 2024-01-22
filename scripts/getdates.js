b8f76b847ce0deb06fdc891e4e4ca5914fd40d78
carrotjived <jakewolfman131@gmail.com>
2024-01-22 16:20:30 +0800

    design-principles

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,5 +3,5 @@ const currentDate = new Date();
 const lastModified = new Date(document.lastModified)
 
 
-document.getElementsByTagName("p")[0].innerHTML = `\u00A9 ${currentDate.getFullYear()} - Victor Jared Onato - Philippines`; 
+document.getElementById("yearName").innerHTML = `\u00A9 ${currentDate.getFullYear()} - Victor Jared Onato - Philippines`; 
 document.getElementById("lastModified").innerHTML = `Last Modification: ${lastModified}`;
