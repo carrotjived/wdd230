6731d98a3cd534bd104afa80fbf6acca50284ee8
carrotjived <jakewolfman131@gmail.com>
2024-01-27 10:27:19 +0800

    added chamber site plan

---
+++ lesson03/scripts/getdates.js
@@ -0,0 +1,7 @@
+
+const currentDate = new Date();
+const lastModified = new Date(document.lastModified)
+
+
+document.getElementById("yearName").innerHTML = `\u00A9 ${currentDate.getFullYear()} - Victor Jared Onato - Philippines`; 
+document.getElementById("lastModified").innerHTML = `Last Modification: ${lastModified}`;
