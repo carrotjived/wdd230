3d7d22e1e9d3b3dcd9ddd6502cc2f022fd42923d
carrotjived <jakewolfman131@gmail.com>
2024-02-23 20:55:15 +0800

    entered page per visit and lazy load the temple

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,5 +3,21 @@ const currentDate = new Date();
 const lastModified = new Date(document.lastModified)
 
 
-document.getElementById("yearName").innerHTML = `\u00A9 ${currentDate.getFullYear()} - Victor Jared Onato - Philippines`; 
+document.getElementById("yearName").innerHTML = `\u00A9 ${currentDate.getFullYear()} - Victor Jared Onato - Philippines`;
 document.getElementById("lastModified").innerHTML = `Last Modification: ${lastModified}`;
+
+const visitsDisplay = document.querySelector('.visits');
+
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+}
+else {
+    visitsDisplay.textContent = 'This is your first visit!';
+
+}
+
+numVisits++;
+
+localStorage.setItem("visits-ls", numVisits)
