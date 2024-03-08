1c91134553c870c574a57a6dba973eb19ae6d4d9
carrotjived <jakewolfman131@gmail.com>
2024-03-09 00:18:12 +0800

    completed links

--- scripts/links.js
+++ scripts/links.js
@@ -1,35 +1,66 @@
-const baseURL = "https://github.com/carrotjived/wdd230";
-const linksURL = "https://github.com/carrotjived/wdd230/blob/main/data/links.json";
+const baseURL = "carrotjived.github.io/wdd230";
+const linksURL = 'https://carrotjived.github.io/wdd230/data/links.json';
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+const container = document.querySelector('.la-card');
+removeAllChildNodes(container);
+
+const learningActivitiesHeader = document.createElement('h3');
+learningActivitiesHeader.textContent = 'Learning Activities';
+
+container.appendChild(learningActivitiesHeader);
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.table(data.lesson);
-    // displayLinks(data);
+
+    displayLinks(data.lessons);
 }
 
-// const displayLinks = (weeks) => {
-//     links.forEach(week => {
-//         const ulElement = document.querySelector(".activities");
-//         ulElement.innerHTML = "";
+const displayLinks = (weeks) => {
+    weeks.forEach(week => {
+        let card = document.createElement('div');
+        let p = document.createElement('p');
 
-//         var a = document.createElement('a');
+        p.textContent = `Week ${week.lesson}: `;
 
-//         var link = document.createTextNode(`${week.links.url}`);
-//         a.appendChild(link);
-//         a.title = `${week.links.title}`;
-//         a.href = `${week.links.url}`;
+        // let a = document.createElement('a');
+        // a.setAttribute("href", week.links[0].url)
+        // a.setAttribute("title", week.links[0].title);
+        // a.textContent = `${week.links[0].title}`;
+
+        week.links.forEach(link => {
+            let a = document.createElement('a');
+            a.setAttribute("href", link.url);
+            a.textContent = `||| ${link.title} `;
+            p.appendChild(a);
 
 
-//         let week = document.createElement('li');
-//         let description = document.createTextNode(`Week ${week.lesson}: `);
-//         week.appendChild(description);
-//         week.appendChild(a);
 
-//         ulElement.appendChild(week);
+        });
 
-//     });
-// }
 
+
+
+
+
+
+
+
+
+        card.appendChild(p);
+
+        container.appendChild(card);
+
+    });
+}
 
 getLinks();
+
+
+
