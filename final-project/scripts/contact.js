e676550b5a6814c97d7baa10069895fa4e6b9e64
carrotjived <jakewolfman131@gmail.com>
2024-04-08 17:17:01 +0800

    finished the project

---
+++ final-project/scripts/contact.js
@@ -0,0 +1,20 @@
+//FOOTER
+
+const currentDate = new Date();
+const lastModified = new Date(document.lastModified);
+
+document.getElementById("yearName").innerHTML = `\u00A9 ${currentDate.getFullYear()} - Victor Jared Onato - WDD 230`;
+document.getElementById("lastModified").innerHTML = `Last Modification: ${lastModified}`;
+
+//BURGER
+
+const mainNav = document.querySelector('.navi');
+const hamButton = document.querySelector('#menu');
+
+
+hamButton.addEventListener('click', () => {
+    mainNav.classList.toggle('open');
+    hamButton.classList.toggle('open');
+
+
+});
